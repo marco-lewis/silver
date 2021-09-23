198a7c9e08fe89c4be198ef20345244490eb0823
Marco Lewis <marco-lewis@hotmail.com>
2021-09-23 15:43:00 +0100

    Minor name changes and notes on future development

--- JSONInterpreter.py
+++ JSONInterpreter.py
@@ -8,7 +8,18 @@ class JSONInterpreter:
 
     def getJSON(self):
         with open(self.file, "r") as rf:
-            self.data = json.load(rf)
+            self.fdefs = rf.read()
+            self.fdefs = json.loads(self.fdefs)
             
     def print_data(self):
-        print(self.data)
+        print(self.fdefs)
+
+    def decode_json(self, dict):
+        # Have functions that contain an array of statements (which may or may not have arrays/objects inside them)
+        # 1) Get function name
+        # 2) Check if there is a summary of it
+        # a) Create the summary if there is one and go to next function
+        # 3) Check if there is already a generated PO file
+        # a) If hash is the same, just use that
+        # 4) Look into statements and generate proof obligations by necessary things from exprType
+        return dict
