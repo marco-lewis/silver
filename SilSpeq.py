4c32d0df72e96f468a6c0659a67b3367f721b258
Marco Lewis <marco-lewis@hotmail.com>
2021-09-29 15:35:07 +0100

    Initial commit with basic spec language and example

---
+++ SilSpeq.py
@@ -0,0 +1,14 @@
+from lark import Lark
+
+with open("SilSpec.lark", 'r') as f:
+    grammar = f.read()
+
+with open("ex.spq", 'r') as f:
+    spec = f.read()
+
+print("Read files")
+parser = Lark(grammar, parser='lalr')
+print("Parser made")
+tree = parser.parse(spec)
+
+print(tree)
