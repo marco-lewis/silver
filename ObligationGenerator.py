e8e779815bbfcf68dd8063123a23e7e8489bb4b8
Marco Lewis <marco-lewis@hotmail.com>
2021-10-13 10:19:03 +0100

    Removed Checker classes and added new obligation generator

---
+++ ObligationGenerator.py
@@ -0,0 +1,25 @@
+from z3 import *
+from QuantumReferencer import QuantumReferencer
+import complex
+
+class ObilgationGenerator:
+
+    qref = QuantumReferencer()
+
+    def __init__(self):
+        pass
+
+    def make_token(self, variable):
+        pass
+
+    def make_assignment(self, lhs, rhs):
+        pass
+
+    def make_literal(self, literal = 0):
+        pass
+
+    def make_operation(self, operation):
+        pass
+
+
+
