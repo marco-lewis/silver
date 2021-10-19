93c104a7f89b3d83931d59146c01eadee90dd7ef
Marco Lewis <marco-lewis@hotmail.com>
2021-10-19 11:56:05 +0100

    Rename SilSpeq to example

---
+++ example.py
@@ -0,0 +1,19 @@
+from lark import Lark
+
+def example():
+    with open("SilSpeq.lark", 'r') as f:
+        grammar = f.read()
+
+    with open("ex.spq", 'r') as f:
+        spec = f.read()
+
+    print("Read files")
+    parser = Lark(grammar, parser='lalr')
+    print("Parser made")
+    tree = parser.parse(spec)
+
+    print(tree)
+
+
+if __name__ == "__main__":
+    example()
