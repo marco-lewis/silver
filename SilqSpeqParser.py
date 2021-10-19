12fb51ac261251db754dbf8ce152685909658aba
Marco Lewis <marco-lewis@hotmail.com>
2021-10-19 12:01:42 +0100

    Created basic parser class

---
+++ SilqSpeqParser.py
@@ -0,0 +1,15 @@
+from lark import Lark
+
+class SilSpeqParser:
+    def __init__(self):
+        with open("SilSpeq.lark", 'r') as f:
+            self.grammar = f.read()
+        self.parser = Lark(self.grammar, parser='larl')
+
+    def parse(self, spec):
+        return self.parser.parse(spec)
+
+    def parse_file(self, file_name):
+        with open(file_name, 'r') as file:
+            spec = file.read()
+        return self.parse(spec)
