ee6e7ad1846da49aba3840b262ceb1051348d572
Marco Lewis <marco-lewis@hotmail.com>
2021-10-20 15:51:21 +0100

    Able to handle basic logical expressions and can add texpressions to the solver

--- SilSpeqInterpreter.py
+++ SilSpeqInterpreter.py
@@ -38,9 +38,10 @@ class SilSpeqInterpreter(Interpreter):
 
 
     # Handling definitions and statements
+    # TODO: Add restrictions on values (e.g. b:{0,1} means s.add(Or(b==0, b==1)))
     @visit_children_decor
     def definition(self, df):
-        var = self.token(df[0])
+        var = df[0].value
         if df[1] == NAT:
             self.vars[var] = Int(var)
         else:
@@ -48,8 +49,9 @@ class SilSpeqInterpreter(Interpreter):
 
 
     @visit_children_decor
-    def assertion(self, expr):
-        return expr
+    def assertion(self, zexpr):
+        print('a', zexpr)
+        self.solver.add(zexpr)
 
 
     # Handling types
@@ -64,27 +66,32 @@ class SilSpeqInterpreter(Interpreter):
 
     # Handling lexpr
     @visit_children_decor
-    def eq(self, a):
-        return a
+    def eq(self, lexprs):
+        return lexprs[0] == lexprs[1]
 
-    # def le(self, a):
-    #     return a[0] <= a[1]
+    @visit_children_decor
+    def le(self, nexprs):
+        return nexprs[0] <= nexprs[1]
 
-    # def land(self, a):
-    #     return And(a[0], a[0])
+    @visit_children_decor
+    def land(self, lexprs):
+        return And(lexprs[0], lexprs[0])
 
-    # def lor(self, a):
-    #     return Or(a[0], a[1])
+    @visit_children_decor
+    def lor(self, lexprs):
+        return Or(lexprs[0], lexprs[1])
 
-    # def implies(self, a):
-    #     return Implies(a[0], a[1])
+    @visit_children_decor
+    def implies(self, lexprs):
+        return Implies(lexprs[0], lexprs[1])
 
-    # def equiv(self, a):
-    #     return a
-        # return Equiv(a[0], a[1])
+    @visit_children_decor
+    def equiv(self, lexprs):
+        return Equiv(lexprs[0], lexprs[1])
     
-    # def lnot(self, a):
-    #     return Not(a[0])
+    @visit_children_decor
+    def lnot(self, lexpr):
+        return Not(lexpr[0])
     
     # def forall(self, a):
     #     return ForAll([a[0]], a[1])
@@ -94,9 +101,12 @@ class SilSpeqInterpreter(Interpreter):
 
     # Handling numexpr
     @visit_children_decor
+    def var(self, expr):
+        return self.token(expr[0])
+    
+    @visit_children_decor
     def add(self,exprs):
         return self.handle_token(exprs[0]) + self.handle_token(exprs[1])
-
 
     @visit_children_decor
     def sub(self,exprs):
@@ -126,12 +136,11 @@ class SilSpeqInterpreter(Interpreter):
         else:
             return t
 
-
     def token(self, tok: Token):
         if tok.type == "NUMBER":
             return self.NUMBER(tok)
         if tok.type == "NAME":
-            return tok.value
+            return self.NAME(tok)
 
     def NUMBER(self, n):
         (n,) = n
