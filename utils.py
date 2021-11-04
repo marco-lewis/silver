26d6464e9469155576bc20c26cceeceb87890d00
Marco Lewis <marco-lewis@hotmail.com>
2021-11-04 16:10:37 +0000

    Added dot product

--- utils.py
+++ utils.py
@@ -17,3 +17,18 @@ def kronecker(matrix1, matrix2):
     """
     return [[num1 * num2 for num1 in elem1 for num2 in matrix2[row]] for elem1 in matrix1 for row in range(len(matrix2))]
  
+def dot(A, B):
+    """
+    Performs matrix multiplication on 2 lists of lists
+    
+    Source:
+        http://www.rosettacode.org/wiki/Matrix_multiplication#Python
+        
+    Args:
+        A (T[][]): First list to be used in operation
+        B (T[][]): Second list to be used in the operation
+
+    Returns:
+        T[][]: A.B
+    """
+    return [[sum(x * B[i][col] for i,x in enumerate(row)) for col in range(len(B[0]))] for row in A]
