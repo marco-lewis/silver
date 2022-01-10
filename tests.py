b9023cb6cc8344de467ca5bd3c38806b5a2e57f0
Marco Lewis <marco-lewis@hotmail.com>
2022-01-10 13:38:20 +0000

    Updated tests.py with expectations

--- tests.py
+++ tests.py
@@ -1,14 +1,18 @@
 from SilVer import SilVer
+import z3
 
-def check(json_file, func, verbose=False):
+def check(json_file, func, expected, verbose=False):
     silver = SilVer()
     sat = silver.verify_func(json_file, func, verbose)
-    print(sat)
+    if sat == expected:
+        print("Test passed as expected")
+    if sat != expected:
+        raise Exception("SatError: Expected " + str(expected) + " but got " + str(sat))
     if verbose:
         print(silver.solver.model())
     
-check("test_singlevar.json", "main")
-check("test_unitary.json", "main")
-check("uint.json", "uint_test")
-check("types.json", "main")
-check("deutsch.json", "deutsch", True)
+check("test_singlevar.json", "main", z3.sat)
+check("test_unitary.json", "main", z3.sat)
+check("uint.json", "uint_test", z3.sat)
+check("types.json", "main", z3.sat)
+check("deutsch.json", "deutsch", z3.unsat, verbose=True)
