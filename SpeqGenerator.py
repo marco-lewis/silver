91b1ae25f7a556a5fce1f4116cf45e788c9afa49
Marco Lewis <marco-lewis@hotmail.com>
2022-01-27 14:27:14 +0000

    Updated SpeqGenerator to handle uint types

--- SpeqGenerator.py
+++ SpeqGenerator.py
@@ -5,25 +5,40 @@ class SpeqGenerator():
     def __init__(self, silq_json, speq_file):
         self.silq_json = silq_json
         self.speq_file = speq_file
-    
+        
+    # TODO: Convert argument types from Silq to JSON format
+    # TODO: Correctly interpret types to speq version
+    # TODO: Make a test library for this    
     def generate_speq_file(self):
         silspeq = ""
         for func_json in self.silq_json:
             fname = func_json['func']
             args = ["define " + arg["name"] + ":" + self.convert_type_to_speq_type(arg["type"])
                     for arg in func_json['args']]
-            ret = "define " + fname + "_ret:{0,1}"
+            ret = "define " + fname + "_ret:" + self.convert_retType(func_json["retType"])
             silspeq += generate_silspeq_from_func(fname, args, ret) + "\n\n"
         silspeq = silspeq[:-2]
         with open(self.speq_file, "w") as wf:
             wf.write(silspeq)
-
-    # TODO: Correctly interpret types to speq version
-    # TODO: Make a test library for this
-    def convert_type_to_speq_type(self, type):
-        if (re.match("[N|ℕ]", type)):
+            
+    def convert_retType(self, retType: dict):
+        type = retType["typeObj"]
+        speq_type = self.convert_type_to_speq_type(type)
+        if type == "uint":
+            size = retType["size"]["value"]
+            speq_type += "^" + str(size)
+        return speq_type
+        
+    def convert_type_to_speq_type(self, type : str):
+        if (re.match(r"𝟙", type)):
+            return "()"
+        if (re.match(r"[N|ℕ]", type)):
             return "N"
-        if (re.match("[B|𝔹]", type)):
+        if (re.match(r"[B|𝔹]", type)):
+            return "{0, 1}"
+        if (re.match(r"uint", type)):
+            if (re.match(r"uint\[[0-9]+\]", type)):
+                return "{0, 1}^" + type.split("[")[1].split("]")[0]
             return "{0, 1}"
         if (re.match(r".*(→.*)+",type)):
             types = [self.convert_type_to_speq_type(arg_type) + "->"
