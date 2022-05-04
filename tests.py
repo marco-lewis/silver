e8d35022be9d54351984a17911cd77fe76b3e370
Marco Lewis <marco-lewis@hotmail.com>
2022-05-04 15:13:47 +0100

    SilSpeq changed so that grover_rev7 now works

--- tests.py
+++ tests.py
@@ -10,8 +10,9 @@ def check(json_file, func, expected, verbose=False, stats=True, timeout=60000):
     if sat == expected: print("Test passed as expected")
     if verbose and sat == z3.sat:
         print("Model/CEX")
-        print(silver.solver.model())
-        try: print(silver.solver.model()['f'])
+        m = silver.solver.model()
+        print(m)
+        try: print('Function: ', m[z3.Function('f', z3.IntSort(), z3.IntSort())])
         except: pass
     if verbose and sat == z3.unsat:
         print("Unsat Core:")
@@ -28,7 +29,7 @@ def check(json_file, func, expected, verbose=False, stats=True, timeout=60000):
 # check("test_singlevar.json", "main", z3.sat)
 # check("test_unitary.json", "main", z3.sat)
 # check("uint.json", "uint_test", z3.sat)
-# check("types.json", "main", z3.sat)
+# check("types.json", "main", z3.sat, True)
 
 # Verification of Deutsch's algorithm
 # check("deutsch.json", "deutsch", z3.unsat)
@@ -45,7 +46,7 @@ def check(json_file, func, expected, verbose=False, stats=True, timeout=60000):
 # check("dj_fixed2.json", "fixed_dj", z3.unsat)
 # check("dj_fixed3.json", "fixed_dj", z3.unsat)
 # check("dj_fixed4.json", "fixed_dj", z3.unsat)
-check("dj_fixed5.json", "fixed_dj", z3.unsat)
+# check("dj_fixed5.json", "fixed_dj", z3.unsat)
 # check("dj_fixed6.json", "fixed_dj", z3.unsat)
 # Very slow (kinda expected)
 # check("dj_fixed7.json", "fixed_dj", z3.unsat)
@@ -53,6 +54,10 @@ check("dj_fixed5.json", "fixed_dj", z3.unsat)
 # Verification of Grover's Algorithm - Work in Progress
 # 2 qubits - uses certainty
 # check("grover_fixed.json", "grover_fixed", z3.unsat)
+# Rev
+# check("grover_rev2.json", "grover_rev", z3.unsat)
+# check("grover_rev3.json", "grover_rev", z3.unsat)
+# check("grover_rev7.json", "grover_rev", z3.unsat)
 # 3 qubits - problems with measurement/probability
 # Likely causing check to be unknown
 # check("grover_fixed2.json", "grover_fixed", z3.unsat)
