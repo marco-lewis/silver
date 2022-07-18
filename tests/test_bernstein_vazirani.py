90f890fba4d6f7b19ef99f5bedf7ff4e898c70f8
Marco Lewis <marco-lewis@hotmail.com>
2022-07-18 15:44:01 +0100

    Added more BV examples

--- tests/test_bernstein_vazirani.py
+++ tests/test_bernstein_vazirani.py
@@ -4,3 +4,6 @@ import z3
 # Verification of Bernstein-Vazirani
 # bv_fixed<n> - Deutsch-Jozsa for n-qubits
 check("bv_fixed2.json", "fixed_bv", z3.unsat)
+check("bv_fixed3.json", "fixed_bv", z3.unsat)
+check("bv_fixed4.json", "fixed_bv", z3.unsat)
+check("bv_fixed5.json", "fixed_bv", z3.unsat)
