4ba57d7d01edca1b0867c7aa0e6bf7172bd0454d
Marco Lewis <marco-lewis@hotmail.com>
2022-07-19 10:14:51 +0100

    Renamed grover files so they reflect number of qubits

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -6,16 +6,16 @@ check("grover_rev2.json", "grover_rev", z3.unsat)
 check("grover_rev3.json", "grover_rev", z3.unsat)
 check("grover_rev7.json", "grover_rev", z3.unsat)
 
-# Verification of Grover's Algorithm
+# Verification of Grover's Algorithm - requires no eq2bv
 # 1 Call
 # 2 qubits - uses certainty
-check("grover_fixed.json", "grover_fixed", z3.unsat)
+check("grover_fixed2.json", "grover_fixed", z3.unsat)
 
 # 2 Calls
 # 3 qubits - problems with measurement/probability
 # Likely causing check to be unknown
 # Setting WHP bound to be 1/2 ignores timeout and causes SilVer to take ~45s
-check("grover_fixed2.json", "grover_fixed", z3.unsat)
+check("grover_fixed3.json", "grover_fixed", z3.unsat)
 # 4 qubits
 # 5 qubits
 # 6 qubits
