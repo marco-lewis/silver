ee655295f15c6a82c7adc8c03f9fe3d8be95cee4
Marco Lewis <marco-lewis@hotmail.com>
2022-07-25 13:53:25 +0100

    More checks for test_grover with new program

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -6,10 +6,11 @@ check("grover_rev2.slq", "grover_rev", z3.unsat)
 check("grover_rev3.slq", "grover_rev", z3.unsat)
 check("grover_rev7.slq", "grover_rev", z3.unsat)
 
-# Verification of Grover's Algorithm - requires no eq2bv
+# Verification of Grover's Algorithm
 # 1 Call
 # 2 qubits - uses certainty
 check("grover_fixed2.slq", "grover_fixed", z3.unsat)
+check("grover_partial3.slq", "grover_partial", z3.unsat)
 
 # 2 Calls
 # 3 qubits - problems with measurement/probability
