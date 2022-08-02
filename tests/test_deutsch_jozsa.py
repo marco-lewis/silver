6c2d3bde8ff7c81bd203d9910eb522ea0926c95d
Marco Lewis <marco-lewis@hotmail.com>
2022-08-02 13:40:19 +0100

    Removed timeout for the moment

--- tests/test_deutsch_jozsa.py
+++ tests/test_deutsch_jozsa.py
@@ -10,6 +10,6 @@ check("dj_fixed3.slq", "fixed_dj", z3.unsat)
 check("dj_fixed4.slq", "fixed_dj", z3.unsat)
 check("dj_fixed5.slq", "fixed_dj", z3.unsat)
 check("dj_fixed6.slq", "fixed_dj", z3.unsat)
-check("dj_fixed7.slq", "fixed_dj", z3.unsat, timeout=60000)
+check("dj_fixed7.slq", "fixed_dj", z3.unsat)
 # Returns Unkown < 180s
 check("dj_fixed8.slq", "fixed_dj", z3.unsat)
