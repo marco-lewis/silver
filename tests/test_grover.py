ca144ada0ea27037c4818d1ec7eee780723924d3
Marco Lewis <marco-lewis@hotmail.com>
2022-08-11 15:49:06 +0100

    Added more grover tests

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -10,19 +10,19 @@ import z3
 # 1 Call
 # 2 qubits - uses certainty
 check("grover_fixed2.slq", "grover_fixed", z3.unsat)
-check("grover_partial3.slq", "grover_partial", z3.unsat, verbose=True)
+check("grover_partial3.slq", "grover_partial", z3.unsat)
 
 # 2 Calls
 # 3 qubits - problems with measurement/probability
 # Likely causing check to be unknown
 # Ran for 5 minutes but no sauce
 # check("grover_err3.slq", "grover_fixed", z3.sat)
-# check("grover_fixed3.slq", "grover_fixed", z3.unsat, verbose=True, timeout=30000)
-# 4 qubits
-# check("grover_fixed4.slq", "grover_fixed", z3.unsat, verbose=True)
-# 5 qubits
-# 6 qubits
-# check("grover_fixed6.slq", "grover_fixed", z3.unsat, verbose=True)
+check("grover_fixed3.slq", "grover_fixed", z3.unsat)
+check("grover_fixed4.slq", "grover_fixed", z3.unsat)
+check("grover_fixed5.slq", "grover_fixed", z3.unsat)
+check("grover_fixed6.slq", "grover_fixed", z3.unsat)
 
 # 3 Calls - 7-14
+check("grover_fixed7.slq", "grover_fixed", z3.unsat)
+
 # 4 Calls - 15- 24
