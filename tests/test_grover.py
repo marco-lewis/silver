9c02c20f78965a254877d6b44678405d3fa0c1cf
Marco Lewis <marco-lewis@hotmail.com>
2022-08-15 11:26:57 +0100

    Deleted an example and update to grover_tests

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -2,9 +2,9 @@ from tests.check import check
 import z3
 
 # Grover Reversed
-# check("grover_rev2.slq", "grover_rev", z3.unsat)
-# check("grover_rev3.slq", "grover_rev", z3.unsat)
-# check("grover_rev7.slq", "grover_rev", z3.unsat)
+check("grover_rev2.slq", "grover_rev", z3.unsat)
+check("grover_rev3.slq", "grover_rev", z3.unsat)
+check("grover_rev7.slq", "grover_rev", z3.unsat)
 
 # Verification of Grover's Algorithm
 # 1 Call
@@ -13,10 +13,9 @@ check("grover_fixed2.slq", "grover_fixed", z3.unsat)
 check("grover_partial3.slq", "grover_partial", z3.unsat)
 
 # 2 Calls
-# 3 qubits - problems with measurement/probability
-# Likely causing check to be unknown
-# Ran for 5 minutes but no sauce
-# check("grover_err3.slq", "grover_fixed", z3.sat)
+# 3 qubits
+# Verifications is fast but finding instance is v. slow
+# TODO: Allow multiple flags (for this prob. bound)
 check("grover_fixed3.slq", "grover_fixed", z3.unsat)
 check("grover_fixed4.slq", "grover_fixed", z3.unsat)
 check("grover_fixed5.slq", "grover_fixed", z3.unsat)
