538f264834dd9cb685969fa36e75c68d3c5334be
Marco Lewis <marco-lewis@hotmail.com>
2023-03-23 16:18:36 +0000

    Removed more files

--- tests/test_basics.py
+++
@@ -1,9 +0,0 @@
-from tests.check import check
-import z3
-
-# Basic checks that SilVer compiles correctly
-# TODO: Create missing files
-check("test_singlevar.slq", "main", z3.sat)
-check("test_unitary.slq", "main", z3.sat)
-check("uint.slq", "uint_test", z3.sat)
-check("types.slq", "main", z3.sat)
