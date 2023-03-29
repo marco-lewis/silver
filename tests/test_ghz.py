a9084f61e5b7c8b76121668cbc1669032b2e8a45
Marco Lewis <marco-lewis@hotmail.com>
2023-03-29 15:37:37 +0100

    Remove log_level change

--- tests/test_ghz.py
+++ tests/test_ghz.py
@@ -3,7 +3,7 @@ from tests.check import check
 import z3
 
 # Verification of GHZ States
-check("ghz2.slq", "ghz", z3.unsat, log_level=logging.DEBUG)
+check("ghz2.slq", "ghz", z3.unsat)
 check("ghz3.slq", "ghz", z3.unsat)
 check("ghz4.slq", "ghz", z3.unsat)
 check("ghz5.slq", "ghz", z3.unsat)
