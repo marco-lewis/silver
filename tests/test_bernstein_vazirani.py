75020a38c606a440c42b9b5c2f6e6bc54857b271
Marco Lewis <marco-lewis@hotmail.com>
2023-03-31 11:22:59 +0100

    Import logging

--- tests/test_bernstein_vazirani.py
+++ tests/test_bernstein_vazirani.py
@@ -1,3 +1,4 @@
+import logging
 from tests.check import check
 import z3
 
@@ -7,7 +8,7 @@ check("bv_fixed2.slq", "fixed_bv", z3.unsat)
 check("bv_fixed3.slq", "fixed_bv", z3.unsat)
 check("bv_fixed4.slq", "fixed_bv", z3.unsat)
 check("bv_fixed5.slq", "fixed_bv", z3.unsat)
-# Takes ~45s for 6 qubit case
+# # Takes ~45s for 6 qubit case
 check("bv_fixed6.slq", "fixed_bv", z3.unsat)
 check("bv_fixed7.slq", "fixed_bv", z3.unsat)
 check("bv_fixed8.slq", "fixed_bv", z3.unsat)
