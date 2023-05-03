a78c275f0a94389b7c96577e99f5a112d1638304
Marco Lewis <marco-lewis@hotmail.com>
2023-05-03 11:14:41 +0100

    Remove TODO

--- tests/test_ctrl.py
+++ tests/test_ctrl.py
@@ -5,6 +5,5 @@ import z3
 check("ctrl1.slq", "ctrl1", z3.unsat)
 check("ctrl2.slq", "ctrl2", z3.unsat)
 check("ctrl3.slq", "ctrl3", z3.unsat)
-# TODO: ctrl_else1 returns unkown
 check("ctrl_else1.slq", "ctrl_else1", z3.unsat)
 check("ctrl_else2.slq", "ctrl_else2", z3.unsat)
