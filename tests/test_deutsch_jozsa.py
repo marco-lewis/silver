97c30a58f8e62da3fbdf59e2ef0052453666de87
Marco Lewis <marco-lewis@hotmail.com>
2023-05-18 16:18:57 +0100

    Removed alternative tests

--- tests/test_deutsch_jozsa.py
+++ tests/test_deutsch_jozsa.py
@@ -6,8 +6,6 @@ import z3
 # Start thinking on BMC
 # Give variable that is being BMC'd and check program with different sizes
 # dj_fixed<n> - Deutsch-Jozsa for n-qubits
-check("dj_fixed2.slq", "fixed_dj", z3.unsat, spq_file=folder+"dj_fixed2const.spq")
-check("dj_fixed2.slq", "fixed_dj", z3.unsat, spq_file=folder+"dj_fixed2bal.spq")
 check("dj_fixed2.slq", "fixed_dj", z3.unsat)
 check("dj_fixed3.slq", "fixed_dj", z3.unsat)
 check("dj_fixed4.slq", "fixed_dj", z3.unsat)
