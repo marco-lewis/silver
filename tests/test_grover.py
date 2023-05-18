43d7cc4e00dd387fcfab218491ede2d3c564082d
Marco Lewis <marco-lewis@hotmail.com>
2023-05-18 16:20:16 +0100

    Update test_grover

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -1,15 +1,16 @@
-from tests.check import check
+import logging
+from tests.check import check, folder
 import z3
 
 # Verification of Grover's Algorithm
 # 1 Call
 # 2 qubits - uses certainty
 check("grover_fixed2.slq", "grover_fixed", z3.unsat)
+check("grover_fixed2.slq", "grover_fixed", z3.unsat, spq_file=folder + "grover_fixed2alt.spq")
+check("grover_partial3.slq", "grover_partial", z3.unsat)
 
 # 2 Calls
-# 3 qubits
 check("grover_fixed3.slq", "grover_fixed", z3.unsat)
-
 # 3 Calls - 4-14?
 check("grover_fixed4.slq", "grover_fixed", z3.unsat)
 check("grover_fixed5.slq", "grover_fixed", z3.unsat)
