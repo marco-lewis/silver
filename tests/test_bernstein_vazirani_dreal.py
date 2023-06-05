6d7e3892df9725269bc2778e2306dabd79ba32ff
Marco Lewis <marco-lewis@hotmail.com>
2023-06-05 15:51:29 +0100

    Fix logging files and log_level

--- tests/test_bernstein_vazirani_dreal.py
+++ tests/test_bernstein_vazirani_dreal.py
@@ -5,15 +5,15 @@ import z3
 
 # Verification of Bernstein-Vazirani
 # bv_fixed<n> - Deutsch-Jozsa for n-qubits
-logger = setup_logger("bv.log")
+logger = setup_logger("bvdreal.log")
 for i in range(2,9):
     logger.info("Checking bv_fixed" + str(i))
     avg_setup, avg_solve = check("bv_fixed" + str(i) + ".slq",
                                  "fixed_bv",
                                  "unsat",
                                  check_store=True,
-                                 log_level=logging.INFO,
+                                 log_level=logging.ERROR,
                                  mode=DREAL,
-                                 runs=1
+                                 runs=10
                                  )
     logger.info("Setup average: %ss, Run average: %ss", str(avg_setup), str(avg_solve))
--- tests/test_deutsch_jozsa_dreal.py
+++ tests/test_deutsch_jozsa_dreal.py
@@ -7,15 +7,15 @@ import z3
 # Start thinking on BMC
 # Give variable that is being BMC'd and check program with different sizes
 # dj_fixed<n> - Deutsch-Jozsa for n-qubits
-logger = setup_logger("dj.log")
+logger = setup_logger("djdreal.log")
 for i in range(2,9):
     logger.info("Checking fixed_dj" + str(i))
     avg_setup, avg_solve = check("dj_fixed" + str(i) + ".slq",
                                  "fixed_dj",
                                  "unsat",
                                  check_store=True,
-                                 log_level=logging.INFO,
+                                 log_level=logging.ERROR,
                                  mode=DREAL,
-                                 runs=1
+                                 runs=10
                                  )
     logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
