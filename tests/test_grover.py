e27af8c5c2f361a1e7db05549ddc24e514c47ba2
Marco Lewis <marco-lewis@hotmail.com>
2023-06-08 15:09:05 +0100

    Update test_grover.py

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -1,18 +1,28 @@
 import logging
 from tests.check import check, folder
+from tests.log_settings import setup_logger
 import z3
 
 # Verification of Grover's Algorithm
+logger = setup_logger("grover.log")
 # 1 Call
 # 2 qubits - uses certainty
-check("grover_fixed2.slq", "grover_fixed", z3.unsat)
-check("grover_fixed2.slq", "grover_fixed", z3.unsat, spq_file=folder + "grover_fixed2alt.spq")
-check("grover_partial3.slq", "grover_partial", z3.unsat)
+avg_setup, avg_solve = check("grover_fixed2.slq", "grover_fixed", z3.unsat, runs=10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("grover_fixed2.slq", "grover_fixed", z3.unsat, spq_file=folder + "grover_fixed2alt.spq", runs=10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+# avg_setup, avg_solve = check("grover_partial3.slq", "grover_partial", z3.unsat, runs=10)
+# logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
 
 # 2 Calls
-check("grover_fixed3.slq", "grover_fixed", z3.unsat)
+avg_setup, avg_solve = check("grover_fixed3.slq", "grover_fixed", z3.unsat)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
 # 3 Calls - 4-14?
-check("grover_fixed4.slq", "grover_fixed", z3.unsat)
-check("grover_fixed5.slq", "grover_fixed", z3.unsat)
-check("grover_fixed6.slq", "grover_fixed", z3.unsat)
-check("grover_fixed7.slq", "grover_fixed", z3.unsat)
+avg_setup, avg_solve = check("grover_fixed4.slq", "grover_fixed", z3.unsat)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("grover_fixed5.slq", "grover_fixed", z3.unsat)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("grover_fixed6.slq", "grover_fixed", z3.unsat)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("grover_fixed7.slq", "grover_fixed", z3.unsat)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
