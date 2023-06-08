8477f6d7ba20ba037e363cadd58a35cef0c64d65
Marco Lewis <marco-lewis@hotmail.com>
2023-06-08 14:05:51 +0100

    Add Hadamard tests

---
+++ tests/test_hadamard.py
@@ -0,0 +1,18 @@
+import logging
+from silver.silver.utils import DREAL, DREAL_UNSAT
+from tests.check import check
+from tests.log_settings import setup_logger
+import z3
+
+logger = setup_logger("had.log")
+avg_setup, avg_solve = check("hadamard5.slq",
+                               "hadamard",
+                               z3.unsat,
+                               runs=10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("hadamard5.slq",
+                               "hadamard",
+                               DREAL_UNSAT,
+                               mode=DREAL,
+                               runs=10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
