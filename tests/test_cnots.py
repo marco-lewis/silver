5303f6b067adb315da850608286ddcd2c7cdb7c6
Marco Lewis <marco-lewis@hotmail.com>
2023-06-08 14:53:51 +0100

    Update controlled tests

--- tests/test_cnots.py
+++ tests/test_cnots.py
@@ -1,6 +1,21 @@
 import logging
 from tests.check import check
+from tests.log_settings import setup_logger
 import z3
 
-check("cnot.slq", "cnot", z3.unsat)
-check("ccnot.slq", "ccnot", z3.unsat)
+logger = setup_logger("had.log")
+avg_setup, avg_solve = check("cnot.slq",
+                             "cnot",
+                             z3.unsat,
+                             runs = 10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("ccnot.slq",
+                            "ccnot",
+                            z3.unsat,
+                            runs = 10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
+avg_setup, avg_solve = check("cz.slq",
+                               "cz",
+                               z3.unsat,
+                               runs=10)
+logger.info("Setup average: %s, Run average: %s", str(avg_setup), str(avg_solve))
