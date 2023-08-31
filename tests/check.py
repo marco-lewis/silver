da33f920b25d665a189b7f8d6f430eb583467c50
Marco Lewis <marco-lewis@hotmail.com>
2023-08-31 13:27:38 +0100

    Remove extra log line

--- tests/check.py
+++ tests/check.py
@@ -10,10 +10,9 @@ import z3
 
 folder = "examples/Silq_Programs/"
 logger = logging.getLogger("check")
-root = logging.getLogger()
 THREE_HOURS = 3 * 60 * 60 * 1000
 
-def check(json_file, func, expected, log_level=logging.INFO, silver_log_level=logging.ERROR, spq_file=None, stats=False, timeout=THREE_HOURS, seed=1, check_store=False, mode=Z3, delta=0.0001, runs=10):
+def check(json_file, func, expected, log_level=logging.INFO, silver_log_level=logging.ERROR, spq_file=None, stats=True, timeout=THREE_HOURS, seed=1, check_store=False, mode=Z3, delta=0.0001, runs=10):
     times = {"setup": [], "solve": []}
     logger.setLevel(log_level)
     logger.info("Starting check on %s in %s", func, json_file)
@@ -21,7 +20,7 @@ def check(json_file, func, expected, log_level=logging.INFO, silver_log_level=lo
     silver = SilVer(timeout=timeout, seed=seed, check_store=check_store)
 
     for i in range(runs):
-        root.info("Run %s", i + 1)
+        logger.info("Run %s", i + 1)
         sat, model = silver.verify_func(folder + json_file, func, log_level=silver_log_level, spq_file=spq_file, mode=mode, delta=delta)
         if sat == expected: logger.info("Test passed as expected")
         else: logger.error("Expected %s but got %s", expected, sat)
@@ -47,7 +46,6 @@ def check(json_file, func, expected, log_level=logging.INFO, silver_log_level=lo
         sys.stdout.flush()
     return times
 
-def report_results(logger, times, runs=10):
+def report_results(logger:logging.Logger, times, runs=10):
     logger.info("Setup average: %s, Run average: %s", str(sum(times["setup"])/runs), str(sum(times["solve"])/runs))
     logger.info("Setup standard deviation: %s, Run standard deviation: %s", str(std(times["setup"])), str(std(times["solve"])))
-    
