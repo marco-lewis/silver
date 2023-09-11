2e42f90d3bdb7754be8944c2c8db88b44de0ba78
Marco Lewis <marco-lewis@hotmail.com>
2023-09-11 13:58:37 +0100

    Update test_grover

--- tests/test_grover.py
+++ tests/test_grover.py
@@ -8,13 +8,13 @@ logger = setup_logger("grover.log")
 # 1 Call
 # 2 qubits - uses certainty
 times = check("grover_fixed2.slq", "grover_fixed", z3.unsat, log_level=logging.ERROR)
-times = check("grover_fixed2.slq", "grover_fixed", z3.unsat, spq_file=folder + "grover_fixed2alt.spq", log_level=logging.ERROR)
+# times = check("grover_fixed2.slq", "grover_fixed", z3.unsat, spq_file=folder + "grover_fixed2alt.spq", log_level=logging.ERROR)
 
-# times = check("grover_partial3.slq", "grover_partial", z3.unsat, )
+times = check("grover_partial3.slq", "grover_partial", z3.unsat, log_level=logging.ERROR)
 
 
 # 2 Calls
-times = check("grover_fixed3.slq", "grover_fixed", z3.unsat, log_level=logging.INFO)
+# times = check("grover_fixed3.slq", "grover_fixed", z3.unsat, log_level=logging.INFO, silver_log_level=logging.INFO)
 
 # 3 Calls - 4-14?
 # times = check("grover_fixed4.slq", "grover_fixed", z3.unsat, log_level=logging.ERROR,)
