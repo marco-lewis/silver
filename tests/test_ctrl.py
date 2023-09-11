3e920aa626e0a07e28cb2c7756ef8db273277b2f
Marco Lewis <marco-lewis@hotmail.com>
2023-09-11 10:53:29 +0100

    Remove test files

--- tests/test_ctrl.py
+++
@@ -1,9 +0,0 @@
-import logging
-from tests.check import check
-import z3
-
-check("ctrl1.slq", "ctrl1", z3.unsat)
-check("ctrl2.slq", "ctrl2", z3.unsat)
-check("ctrl3.slq", "ctrl3", z3.unsat)
-check("ctrl_else1.slq", "ctrl_else1", z3.unsat)
-check("ctrl_else2.slq", "ctrl_else2", z3.unsat)
--- tests/test_simon.py
+++
@@ -1,7 +0,0 @@
-import logging
-from tests.check import check
-import z3
-
-# Verification of Simon
-# simon<n> - Simon for n-qubits
-check("simon2.slq", "simon", z3.unsat)
